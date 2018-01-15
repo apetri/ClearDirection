338ebf10ff83ee84344919676678436008603e68
apetri <apetri@phys.columbia.edu>
2018-01-15 15:08:16 -0500

    sample table

--- cleardirection/urls.py
+++ cleardirection/urls.py
@@ -3,5 +3,6 @@ from django.contrib import admin
 
 urlpatterns = [
 	path(r'hello/',include('hello.urls')),
+	path(r'lookup/',include('lookup.urls')),
     path(r'admin/', admin.site.urls),
 ]
--- lookup/views.py
+++ lookup/views.py
@@ -1,3 +1,13 @@
+#django
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Person
 
 # Create your views here.
+def sampletable(request):
+
+	fields = ["username","first","last","age","street_address","city","zipcode","email","secret","isreal"]
+	table = [ [getattr(p,c) for c in fields] for p in Person.objects.all() ]
+	context = {"title":"Sample table","columns":fields,"table":table}
+
+	return HttpResponse(render(request,"lookup/table.html",context))
