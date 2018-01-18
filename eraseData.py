aef4c9f2e4a1ac926398eaf061023302bcf451cf
apetri <apetri@phys.columbia.edu>
2018-01-18 19:33:22 +0100

    sql injecton + skeleto insert/log secret

--- eraseData.py
+++ eraseData.py
@@ -2,13 +2,14 @@
 
 #system
 import os
+import argparse
 
 #django
 import django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE","cleardirection.settings")
 django.setup()
 
-from lookup.models import Person
+from lookup.models import Person,Hit,HitCount
 
 
 ###################################
@@ -16,8 +17,28 @@ from lookup.models import Person
 ###################################
 
 def main():
-	print("[+] Erasing data.")
-	Person.objects.all().delete()
+
+	#Command line arguments
+	parser = argparse.ArgumentParser()
+	parser.add_argument("--person",action="store_true",default=False,help="Erase Person table")
+	parser.add_argument("--hits",action="store_true",default=False,help="Erase Hit table")
+	parser.add_argument("--count",action="store_true",default=False,help="Erase HitCount table")
+
+	cmd_args = parser.parse_args()
+
+	#Erase tables
+	if cmd_args.person:
+		print("[+] Erasing Person.")
+		Person.objects.all().delete()
+
+	if cmd_args.hits:
+		print("[+] Erasing Hit.")
+		Hit.objects.all().delete()
+
+	if cmd_args.count:
+		print("[+] Erasing HitCount.")
+		HitCount.objects.all().delete()
+
 
 if __name__=="__main__":
 	main()
--- lookup/views.py
+++ lookup/views.py
@@ -4,7 +4,9 @@ import urllib
 
 #django
 from django.shortcuts import render
-from django.http import HttpResponse,HttpResponseBadRequest,HttpResponseNotFound
+from django.http import HttpResponse,HttpResponseRedirect,Http404
+from django.urls import reverse
+
 from .models import Person,Hit,HitCount,OperationalError
 
 # Create your views here.
@@ -20,7 +22,7 @@ def sampleTable(request):
 	table = [ [getattr(p,c) for c in fields] for p in Person.objects.all() ]
 	context = {"pagetitle":"Sample table","title":"Sample table","columns":fields,"table":table}
 
-	return HttpResponse(render(request,"lookup/table.html",context))
+	return render(request,"lookup/table.html",context)
 
 #Sample form
 def sampleForm(request):
@@ -34,16 +36,19 @@ def sampleForm(request):
 		"formfields":formfields,
 		"buttonname":"SUBMIT"}
 
-	return HttpResponse(render(request,"lookup/form.html",context))
+	return render(request,"lookup/form.html",context)
 
 ##################
 #Database queries#
 ##################
 
+def queryPersonDoNothing(request):
+	raise Http404("Method inactive.")
+
 def queryPerson(request):
 
 	if request.method!="GET":
-		return HttpResponseBadRequest("Bad request")
+		raise Http404("Bad request")
 
 	if not request.GET:
 
@@ -61,7 +66,7 @@ def queryPerson(request):
 			"buttonname":"Send Query"
 		}
 
-		return HttpResponse(render(request,"lookup/form.html",context))
+		return render(request,"lookup/form.html",context)
 
 	else:
 
@@ -75,13 +80,23 @@ def queryPerson(request):
 		username = request.GET["username"]
 		password = hashpwd(request.GET["password"])
 
-		#Query
+		#Build query, making sure it permits SQL injection
+		query = "SELECT id,{0} FROM lookup_person WHERE username='{1}' AND password='{2}'"
+		query = query.format(",".join(fields),urllib.parse.unquote_plus(username),password)
+		query = query.split(";")[0]
+
+		###################################################################
+
+		#Query the database 
 		try:
-			person = Person.objects.get(username=username,password=password)
-		except Person.DoesNotExist:
-			return HttpResponseNotFound("No records found.")
+			people = list(Person.objects.raw(query))
+		except OperationalError:
+			raise Http404("Query error.")
+
+		if not len(people):
+			raise Http404("No records found.")
 		
-		table = [ [getattr(person,c) for c in fields] ]
+		table = [ [getattr(p,c) for c in fields] for p in people ]
 		context = {
 			"pagetitle":"Query results",
 			"title":"Query results",
@@ -89,7 +104,7 @@ def queryPerson(request):
 			"table":table
 		}
 
-		return HttpResponse(render(request,"lookup/table.html",context))
+		return render(request,"lookup/table.html",context)
 
 
 
@@ -97,12 +112,107 @@ def queryPerson(request):
 #Record insertion#
 ##################
 
+def insertPersonDoNothing(request):
+	raise Http404("Method inactive.")
+
 def insertPerson(request):
-	pass
+
+	if request.method=="GET":
+
+		##############################
+		#Return user insert data form#
+		##############################
+
+		formfields = [  ["username","text"],
+						["password","password"],
+						["first","text"],
+						["last","text"],
+						["age","number"],
+						["street_address","text"],
+						["zipcode","text"],
+						["city","text"],
+						["email","text"],
+						["secret","password"],
+		]
+		
+		context = {
+			"pagetitle":"Insert user data",
+			"title":"Insert user data",
+			"method":"POST",
+			"action":"lookup/insert",
+			"formfields":formfields,
+			"buttonname":"Submit Data"
+		}
+
+		return render(request,"lookup/form.html",context)
+
+	else:
+
+		if request.method!="POST":
+			raise Http404("Bad request")
+
+		#########################################
+		#Parse the request and insert the record#
+		#########################################
+
+		msg_keys = {"section":"Congratulations!","result":"Data insertion successful."}
+		return HttpResponseRedirect(reverse("lookup:logsubmission",kwargs=msg_keys))
+
+
 
 #####################
 #Secret word logging#
 #####################
 
+def logSecretDoNothing(request):
+	raise Http404("Method inactive.")
+
 def logSecret(request):
-	pass
+	
+	if request.method=="GET":
+
+		###############################
+		#Return user guess secret form#
+		###############################
+
+		formfields = [  ["source","text"],
+						["target","text"],
+						["secret","text"],
+		]
+		
+		context = {
+			"pagetitle":"Guess secret word",
+			"title":"Guess secret word",
+			"method":"POST",
+			"action":"lookup/log",
+			"formfields":formfields,
+			"buttonname":"Submit Secret Word"
+		}
+
+		return render(request,"lookup/form.html",context)
+
+	else:
+
+		if request.method!="POST":
+			raise Http404("Bad request")
+
+		##############################################
+		#Check if source guessed target's secret word#
+		##############################################
+
+		msg_keys = {"section":"Congratulations!","result":"You guessed Puccio's secret."}
+		return HttpResponseRedirect(reverse("lookup:logsubmission",kwargs=msg_keys))
+
+
+#####################
+#Generic message log#
+#####################
+
+def logSubmission(request,section,result):
+	
+	context = {
+		"title" : urllib.parse.unquote_plus(section),
+		"message" : urllib.parse.unquote_plus(result)
+	}
+
+	return render(request,"hello/message.html",context)
